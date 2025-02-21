d19597907099269120264508fd278ba54e6adf60
Ruan-Moraes <ruanmoraessantosbarbosa@gmail.com>
2025-02-21 14:07:44 -0300

    Finalizado o teste de tecnologia

--- apiToDo/Program.cs
+++ apiToDo/Program.cs
@@ -11,6 +11,8 @@ namespace apiToDo
 {
     public class Program
     {
+        // Teste finalizado.
+        
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
